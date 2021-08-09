3a2fb223e87424cde58f2988916b73823edfe5ff
Yunzhi Gao <gaoyunzhi@gmail.com>
2021-08-08 17:35:49 -0700

    commit

--- text_2_img/__init__.py
+++ text_2_img/__init__.py
@@ -8,19 +8,28 @@ import re
 from PIL import Image, ImageDraw, ImageFont
 import cjkwrap
 import hashlib
+from reportlab.platypus import Paragraph
+from reportlab.lib.styles import ParagraphStyle, getSampleStyleSheet
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+
+otf_loc = '~/Library/Fonts/SourceHanSerifSC-Light.otf' # 思源宋体
+fft_loc = 'SourceHanSerifCN-Light.ttf'
+# styles = getSampleStyleSheet()
+# style = ParagraphStyle(fontName='myfont', name='myfont', leading=20, fontSize=200)
+# styles.add(style)
 
 def getFilename(text, dirname = 'tmp'):
     text = re.sub(r'[-\s]+', '', text)
     h = hashlib.sha224(text.encode('utf-8')).hexdigest()[:3]
     return text[:10] + '_' + h
 
-other_font_loc = '~/Library/Fonts/SourceHanSerifSC-Light.otf' # 思源宋体
-
-def splitText(text, line_char_max, line_max):
+def splitText(text, line_char_max, line_max, font, width, style):
     text = text.strip()
     lines = []
     for line in text.split('\n'):
-        new_lines = cjkwrap.fill(line, line_char_max).split('\n')
+        new_lines = Paragraph(line, style).breakLinesCJK(width).lines
+        new_lines = [item[1][0] for item in new_lines] or ['']
         if len(lines) + len(new_lines) > line_max:
             yield '\n'.join(lines).strip()
             lines = []
@@ -31,21 +40,23 @@ def splitText(text, line_char_max, line_max):
     if last:
         yield last
 
-def gen(text, dirname = 'tmp', font_loc=other_font_loc, color=(0, 0, 0), 
+def gen(text, dirname = 'tmp', font_loc=otf_loc, fft_loc = fft_loc, color=(0, 0, 0), 
         background=(252, 250, 222), img_size=(3600, 6400), margin=200,
-        font_size=160, padding=10, line_char_max=39, line_max=30):
+        font_size=160, padding=20, line_char_max=39, line_max=28):
+    pdfmetrics.registerFont(TTFont('myfont', fft_loc))  #注册字体
+    style = ParagraphStyle(fontName='myfont', name='myfont', fontSize=font_size)
     os.system('mkdir %s > /dev/null 2>&1' % dirname)
     fn_base = dirname + '/' + getFilename(text)
+    font = ImageFont.truetype(font_loc, font_size)
     result = []
-    texts = list(splitText(text, line_char_max, line_max))
+    texts = list(splitText(text, line_char_max, line_max, font, img_size[0] - margin * 2.5, style))
     for index, subText in enumerate(texts):
         lines = subText.split('\n')
-        font = ImageFont.truetype(font_loc, font_size)
         height = margin
         text_height = font.getsize(lines[0])[1]
         if len(texts) == 1:
             img_size = (img_size[0], 
-                len(lines) * (padding + text_height) + margin * 3)
+                len(lines) * (padding + text_height) + margin * 2.5)
         img = Image.new('RGB', img_size, color=background)
         for line in lines:
             ImageDraw.Draw(img).text((margin, height), line, font=font, fill=color)
